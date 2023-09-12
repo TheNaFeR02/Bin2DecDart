3b611cf9a2c1398f3e356e39ad0be1d0c50f69cb
TheNaFeR02 <acunafer.02@gmail.com>
2023-09-12 17:59:01 -0500

    first commit

---
+++ lib/utils.dart
@@ -0,0 +1,10 @@
+abstract class Utils {
+  static String bin2dec(String binary) {
+    // todo: convierte [binary] en un [int] de base 2 y luego conviertelo a base 10
+    return int.parse(binary, radix: 2).toRadixString(10);
+  }
+
+  static String dec2bin(String decimal) {
+    return int.parse(decimal, radix: 10).toRadixString(2);
+  }
+}
